c75aa3741f84a4f47a35b68622c4701cb4bde885
OwenC03 <owen.carson@edu.sait.ca>
2024-03-24 19:34:29 -0600

    First Push

---
+++ ConnectingCodetoaDatabase/Program.cs
@@ -0,0 +1,17 @@
+﻿//Add MySql Library
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using MySqlConnector;
+
+namespace ConnectingCodetoaDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+        }
+    }
+}
